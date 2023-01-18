2cfd96410b924e9170859cd94772362892d12f8a
KellyVasconcellos <kelly.h.vasconcellos@gmail.com>
2023-01-17 23:25:21 -0300

    feat: adicionando component menu-box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { MenuBoxComponent } from './components/menu-box/menu-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuBoxComponent
   ],
   imports: [
     BrowserModule,
