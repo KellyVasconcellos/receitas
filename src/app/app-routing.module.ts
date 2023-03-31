71ae048e5ddb34a04c74672a4ead6d92267c2f6c
KellyVasconcellos <kelly.h.vasconcellos@gmail.com>
2023-03-31 20:00:00 -0300

    feat: criando formulario de editar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { ReceitaComponent } from './views/receita/receita.component';
+import { InputReceitaComponent } from './views/input-receita/input-receita.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'home/:id', component: ReceitaComponent }
+  { path: 'home/:id', component: ReceitaComponent },
+  { path: 'input', component: InputReceitaComponent }
 ];
 
 @NgModule({
